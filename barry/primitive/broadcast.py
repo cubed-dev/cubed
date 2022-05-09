b5ee401c895efa3d228142b4015dac9ba87fb5dc
Tom White <tom.e.white@gmail.com>
2022-05-09 16:28:31 +0100

    Allow specification of chunks in broadcast_to

--- barry/primitive/broadcast.py
+++ barry/primitive/broadcast.py
@@ -1,12 +1,12 @@
+import numpy as np
 from zarr.core import Array
 
-# TODO: support 'chunks' parameter to allow broadcast dimensions to have arbitrary chunk sizes. (This will require work in Zarr.)
 
-
-def broadcast_to(source, shape):
+def broadcast_to(source, shape, chunks=None):
     """Broadcast an array to a specified shape.
 
-    The broadcast array will have a chunksize of 1 in the broadcast dimensions.
+    The broadcast array will have a chunksize of 1 in the broadcast dimensions, unless the `chunks`
+    parameter is specified.
 
     The returned array is a view of a Zarr array. No extra storage or computation is required.
 
@@ -15,7 +15,12 @@ def broadcast_to(source, shape):
     source : Zarr array
     shape : tuple
         The array shape to be broadcast to.
-
+    chunks : tuple, optional
+        If provided, then the result will use these chunks instead of the same
+        chunks as the source array. Setting chunks explicitly as part of
+        broadcast_to is more efficient than rechunking afterwards. Chunks are
+        only allowed to differ from the original shape along dimensions that
+        are new on the result or have size 1 the input array.
     Returns
     -------
     target:  Array for the Zarr array output
@@ -36,7 +41,17 @@ def broadcast_to(source, shape):
     partial_decompress = False
     write_empty_chunks = True
 
-    chunks = (1,) * ndim_new + source.chunks
+    if chunks is None:
+        chunks = (1,) * ndim_new + source.chunks
+    else:
+        for old_bd, new_bd in zip(source.chunks, chunks[ndim_new:]):
+            if old_bd != new_bd and old_bd != 1:
+                raise ValueError(
+                    "cannot broadcast chunks %s to chunks %s: "
+                    "new chunks must either be along a new "
+                    "dimension or a dimension of size 1" % (source.chunks, chunks)
+                )
+
     bds = [i for i, s in enumerate(source.shape) if s != shape[ndim_new:][i]]
 
     a = BroadcastArray(
@@ -51,6 +66,7 @@ def broadcast_to(source, shape):
         write_empty_chunks=write_empty_chunks,
         shape=shape,
         chunks=chunks,
+        original_chunks=source.chunks,
         bds=bds,
         ndim_new=ndim_new,
     )
@@ -72,6 +88,7 @@ class BroadcastArray(Array):
         write_empty_chunks,
         shape,
         chunks,
+        original_chunks,
         bds,
         ndim_new,
     ):
@@ -89,6 +106,7 @@ class BroadcastArray(Array):
         self._is_view = True
         self._shape = shape
         self._chunks = chunks
+        self._original_chunks = original_chunks
         self._bds = bds
         self._ndim_new = ndim_new
 
@@ -105,14 +123,24 @@ class BroadcastArray(Array):
             self._write_empty_chunks,
             self._shape,
             self._chunks,
+            self._original_chunks,
             self._bds,
             self._ndim_new,
         )
 
     def _chunk_key(self, chunk_coords):
-        # change chunk_coords to pre-broadcast values
+        # change chunk_coords to original (pre-broadcast) values
         chunk_coords = tuple(
             0 if i in self._bds else c
             for i, c in enumerate(chunk_coords[self._ndim_new :])
         )
         return super()._chunk_key(chunk_coords)
+
+    def _decode_chunk(self, cdata, start=None, nitems=None, expected_shape=None):
+        # broadcast the decoded chunk to the broadcast array chunk size
+        if expected_shape is not None:
+            raise NotImplementedError("BroadcastArray does not support expected_shape")
+        chunk = super()._decode_chunk(
+            cdata, start=start, nitems=nitems, expected_shape=self._original_chunks
+        )
+        return np.broadcast_to(chunk, shape=self._chunks)
--- barry/tests/primitive/test_broadcast.py
+++ barry/tests/primitive/test_broadcast.py
@@ -28,3 +28,47 @@ def test_broadcast_to(tmp_path, shape, chunks, new_shape, new_chunks_expected):
     assert target.shape == new_shape
     assert target.chunks == new_chunks_expected
     assert_array_equal(target[:], np.broadcast_to(x, new_shape))
+
+
+@pytest.mark.parametrize(
+    "shape, chunks, new_shape, new_chunks, new_chunks_expected",
+    [
+        ((5, 1, 6), (3, 1, 3), (5, 3, 6), (3, 1, 3), (3, 1, 3)),
+        ((5, 1, 6), (3, 1, 3), (5, 3, 6), (3, 3, 3), (3, 3, 3)),
+        ((5, 1, 6), (3, 1, 3), (2, 5, 3, 6), (1, 3, 1, 3), (1, 3, 1, 3)),
+    ],
+)
+def test_broadcast_to_chunks(
+    tmp_path, shape, chunks, new_shape, new_chunks, new_chunks_expected
+):
+    x = np.random.randint(10, size=shape)
+    source = create_zarr(
+        x,
+        dtype=int,
+        chunks=chunks,
+        store=tmp_path / "source.zarr",
+    )
+    target = broadcast_to(source, new_shape, chunks=new_chunks)
+
+    assert target.shape == new_shape
+    assert target.chunks == new_chunks_expected
+    assert_array_equal(target[:], np.broadcast_to(x, new_shape))
+
+
+def test_broadcast_to_errors(tmp_path):
+    x = np.random.randint(10, size=(5, 1, 6))
+    source = create_zarr(
+        x,
+        dtype=int,
+        chunks=(3, 1, 3),
+        store=tmp_path / "source.zarr",
+    )
+
+    with pytest.raises(ValueError):
+        broadcast_to(source, (2, 1, 6))
+    with pytest.raises(ValueError):
+        broadcast_to(source, (3,))
+
+    # can't rechunk existing (non-broadcast) dimensions
+    with pytest.raises(ValueError):
+        broadcast_to(source, source.shape, chunks=(2, 1, 3))
