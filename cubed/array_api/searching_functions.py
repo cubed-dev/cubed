669431e3036fe911137e568c82e15b890deaff51
Tom White <tom.e.white@gmail.com>
2022-06-27 22:22:17 +0100

    Fix 'where' dtype

--- cubed/array_api/searching_functions.py
+++ cubed/array_api/searching_functions.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+from cubed.array_api.data_type_functions import result_type
 from cubed.core.ops import elemwise
 
 
 def where(condition, x1, x2, /):
-    return elemwise(np.where, condition, x1, x2)
+    dtype = result_type(x1, x2)
+    return elemwise(np.where, condition, x1, x2, dtype=dtype)
