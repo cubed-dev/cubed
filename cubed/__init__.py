985dc03138147e255fd4141d26e384a73c0f9a92
Tom White <tom.e.white@gmail.com>
2022-07-05 11:28:49 +0100

    Expose __version__

--- cubed/__init__.py
+++ cubed/__init__.py
@@ -5,6 +5,13 @@ import warnings
 if not sys.warnoptions:
     warnings.filterwarnings("ignore", category=UserWarning)
 
+from importlib.metadata import version as _version
+
+try:
+    __version__ = _version("cubed")
+except Exception:  # pragma: no cover
+    __version__ = "unknown"
+
 from .core import (
     Callback,
     Spec,
@@ -16,6 +23,7 @@ from .core import (
 )
 
 __all__ = [
+    "__version__",
     "Callback",
     "Spec",
     "TqdmProgressBar",
