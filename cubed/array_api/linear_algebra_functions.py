400dc9adcf21c8b468fce9f24e8d4b8cb9ef2f11
Tom White <tom.e.white@gmail.com>
2022-07-07 11:04:16 +0100

    More matmul cases

--- cubed/array_api/linear_algebra_functions.py
+++ cubed/array_api/linear_algebra_functions.py
@@ -1,13 +1,32 @@
 import numpy as np
 
 from cubed.array_api.data_type_functions import result_type
+from cubed.array_api.dtypes import _numeric_dtypes
+from cubed.array_api.manipulation_functions import expand_dims
 from cubed.core import blockwise, reduction, squeeze
 
 
 def matmul(x1, x2, /):
-    assert x1.ndim >= 2
-    assert x2.ndim >= 2
-    assert x1.ndim == x2.ndim
+    if x1.dtype not in _numeric_dtypes or x2.dtype not in _numeric_dtypes:
+        raise TypeError("Only numeric dtypes are allowed in matmul")
+
+    if x1.ndim == 0 or x2.ndim == 0:
+        raise ValueError("matmul does not support 0-dimensional arrays.")
+
+    x1_is_1d = False
+    if x1.ndim == 1:
+        x1_is_1d = True
+        x1 = expand_dims(x1, axis=0)
+
+    x2_is_1d = False
+    if x2.ndim == 1:
+        x2_is_1d = True
+        x2 = expand_dims(x2, axis=-1)
+
+    if x1.ndim < x2.ndim:
+        x1 = expand_dims(x1, tuple(range(x2.ndim - x1.ndim)))
+    elif x1.ndim > x2.ndim:
+        x2 = expand_dims(x2, tuple(range(x1.ndim - x2.ndim)))
 
     out_ind = tuple(range(x1.ndim + 1))
     lhs_ind = tuple(range(x1.ndim))
@@ -27,6 +46,11 @@ def matmul(x1, x2, /):
     )
 
     out = _sum_wo_cat(out, axis=-2, dtype=dtype)
+
+    if x1_is_1d:
+        out = squeeze(out, -2)
+    if x2_is_1d:
+        out = squeeze(out, -1)
 
     return out
 
