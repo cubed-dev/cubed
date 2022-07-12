14bc1344f157f1214c7e5100c5c6d3f326abdfd5
Tom White <tom.e.white@gmail.com>
2022-07-12 16:03:04 +0100

    Record event history in CSV files at end of computation

---
+++ cubed/extensions/history.py
@@ -0,0 +1,38 @@
+import time
+from dataclasses import asdict
+from pathlib import Path
+
+import networkx as nx
+import pandas as pd
+
+from cubed.core.array import Callback
+from cubed.runtime.pipeline import already_computed
+
+
+class HistoryCallback(Callback):
+    def on_compute_start(self, arr):
+        dag = arr.plan.optimize(arr.name)
+        nodes = {n: d for (n, d) in dag.nodes(data=True)}
+        plan = []
+        for node in list(nx.topological_sort(dag)):
+            if already_computed(nodes[node]):
+                continue
+            required_mem = nodes[node]["required_mem"]
+            num_tasks = nodes[node]["num_tasks"]
+            plan.append(
+                dict(array_name=node, required_mem=required_mem, num_tasks=num_tasks)
+            )
+
+        self.plan = plan
+        self.stats = []
+
+    def on_task_end(self, event):
+        self.stats.append(asdict(event))
+
+    def on_compute_end(self, arr):
+        plan_df = pd.DataFrame(self.plan)
+        stats_df = pd.DataFrame(self.stats)
+        id = int(time.time())
+        Path("history").mkdir(exist_ok=True)
+        plan_df.to_csv(f"history/plan-{id}.csv", index=False)
+        stats_df.to_csv(f"history/stats-{id}.csv", index=False)
