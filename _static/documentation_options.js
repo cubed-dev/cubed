86919fc79aa8d32488e2f07764eae52a3a417a6a
GitHub Action <action@github.com>
2022-08-23 10:49:43 +0000

    Add changes for f8bad35d5abcd398fc5363b2b61841d0b5990056

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0-36-g3413409',
+    VERSION: '0.2.0-37-gf8bad35',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
