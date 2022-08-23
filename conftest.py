3442c31fb806c7c6396f122f6eaf134efd499fcc
Tom White <tom.e.white@gmail.com>
2022-08-23 18:09:23 +0100

    Fix I/O logging error during test suite

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 # content of conftest.py
+import logging
 
 import pytest
 
@@ -21,3 +22,15 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if "cloud" in item.keywords:
             item.add_marker(skip_cloud)
+
+
+# from https://github.com/streamlit/streamlit/pull/5047/files
+def pytest_sessionfinish():
+    # We're not waiting for scriptrunner threads to cleanly close before ending the PyTest,
+    # which results in raised exception ValueError: I/O operation on closed file.
+    # This is well known issue in PyTest, check out these discussions for more:
+    # * https://github.com/pytest-dev/pytest/issues/5502
+    # * https://github.com/pytest-dev/pytest/issues/5282
+    # To prevent the exception from being raised on pytest_sessionfinish
+    # we disable exception raising in logging module
+    logging.raiseExceptions = False
