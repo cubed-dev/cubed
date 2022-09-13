aa767bf523a4cd9f3cec1d88d1381493df036ca5
GitHub Action <action@github.com>
2022-09-13 08:12:24 +0000

    Add changes for 1adef3dbc16ec96a156090711de23db726d181e2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0-7-g04e00bc',
+    VERSION: '0.3.0-8-g1adef3d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
