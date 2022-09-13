3399e6aa8a7e3da31ea0b2b56bfc3522ef91a368
Tom White <tom.e.white@gmail.com>
2022-09-13 15:04:50 +0100

    f

--- cubed/tests/test_fuse.py
+++ cubed/tests/test_fuse.py
@@ -26,6 +26,21 @@ def compute_dag(dag, array_name):
     return nodes[array_name]["array"]
 
 
+def visualize_dag(dag, filename="cubed-fuse", format=None):
+    dag.graph["graph"] = {"rankdir": "TB"}
+    dag.graph["node"] = {"fontname": "helvetica", "shape": "box"}
+    for (n, d) in dag.nodes(data=True):
+        if "func" in d:
+            d["label"] = f"{n} ({d['func'].__name__})"
+        else:
+            d["label"] = f"{n} ({d['array']})"
+    gv = nx.drawing.nx_pydot.to_pydot(dag)
+    if format is None:
+        format = "svg"
+    full_filename = f"{filename}.{format}"
+    gv.write(full_filename, format=format)
+
+
 def test_one_op():
     # b = a + 1
     dag = nx.MultiDiGraph()
@@ -34,6 +49,8 @@ def test_one_op():
 
     dag.add_node("b", func=add_one)
     dag.add_edge("a", "b")
+
+    visualize_dag(dag, filename="cubed-fuse-one")
 
     res = compute_dag(dag, "b")
     assert res == [2, 3, 4, 5]
@@ -52,6 +69,8 @@ def test_two_ops():
     dag.add_node("c", func=add_two)
     dag.add_edge("b", "c")
 
+    visualize_dag(dag, filename="cubed-fuse-two")
+
     res = compute_dag(dag, "c")
     assert res == [4, 5, 6, 7]
 
@@ -69,6 +88,8 @@ def test_complex():
     dag.add_node("c", func=times)
     dag.add_edge("a", "c")
     dag.add_edge("b", "c")
+
+    visualize_dag(dag, filename="cubed-fuse-complex")
 
     res = compute_dag(dag, "c")
     assert res == [2, 6, 12, 20]
