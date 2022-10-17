16c6f2104b855d8225923f785aa58002ea628115
Tom White <tom.e.white@gmail.com>
2022-10-17 17:39:38 +0100

    Embed images in slides

--- docs/conf.py
+++ docs/conf.py
@@ -118,3 +118,5 @@ html_title = "Cubed"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
+
+html_extra_path = ["../examples/cubed.slides.html"]
