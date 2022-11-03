774074fc9c786f25674d8d07642a6213681ae177
GitHub Action <action@github.com>
2022-11-03 21:06:45 +0000

    Add changes for 3996d9334fbd260e63210011ec668ef98f871190

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1-3-g1ab1f0f',
+    VERSION: '0.4.1-2-g3996d93',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
