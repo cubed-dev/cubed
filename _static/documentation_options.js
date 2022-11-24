eb21afd03119b95da364de273555833437d27cfd
GitHub Action <action@github.com>
2022-11-24 08:44:20 +0000

    Add changes for deb6d2b136e8b06cc9e6ef2b4873b1ae9cd5c252

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.1-2-g3996d93',
+    VERSION: '0.4.1-3-gdeb6d2b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
