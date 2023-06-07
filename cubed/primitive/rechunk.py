ed397bd0a62207e4ca27cc3b7dd03387340dba88
Tom White <tom.e.white@gmail.com>
2023-06-07 11:51:05 +0100

    Implement internal rechunk function, so we only depend on rechunker for the rechunking algorithm.

--- cubed/primitive/rechunk.py
+++ cubed/primitive/rechunk.py
@@ -1,7 +1,17 @@
 from math import ceil, prod
 
+import zarr
+
+import cubed
 from cubed.runtime.pipeline import spec_to_pipeline
-from cubed.vendor.rechunker.api import _setup_rechunk
+from cubed.vendor.rechunker.algorithm import rechunking_plan
+from cubed.vendor.rechunker.api import (
+    _get_dims_from_zarr_array,
+    _shape_dict_to_tuple,
+    _validate_options,
+    _zarr_empty,
+)
+from cubed.vendor.rechunker.types import ArrayProxy, CopySpec
 
 
 def rechunk(
@@ -52,6 +62,129 @@ def rechunk(
     # we assume that rechunker is going to use all the memory it is allowed to
     projected_mem = allowed_mem
     return spec_to_pipeline(copy_spec, target, projected_mem, num_tasks)
+
+
+# from rechunker, but simpler since it only has to handle Zarr arrays
+def _setup_rechunk(
+    source,
+    target_chunks,
+    max_mem,
+    target_store,
+    target_options=None,
+    temp_store=None,
+    temp_options=None,
+):
+    if temp_options is None:
+        temp_options = target_options
+    target_options = target_options or {}
+    temp_options = temp_options or {}
+
+    copy_spec = _setup_array_rechunk(
+        source,
+        target_chunks,
+        max_mem,
+        target_store,
+        target_options=target_options,
+        temp_store_or_group=temp_store,
+        temp_options=temp_options,
+    )
+    intermediate = copy_spec.intermediate.array
+    target = copy_spec.write.array
+    return [copy_spec], intermediate, target
+
+
+def _setup_array_rechunk(
+    source_array,
+    target_chunks,
+    max_mem,
+    target_store_or_group,
+    target_options=None,
+    temp_store_or_group=None,
+    temp_options=None,
+    name=None,
+) -> CopySpec:
+    _validate_options(target_options)
+    _validate_options(temp_options)
+    shape = source_array.shape
+    # source_chunks = (
+    #     source_array.chunksize
+    #     if isinstance(source_array, dask.array.Array)
+    #     else source_array.chunks
+    # )
+    source_chunks = source_array.chunks
+    dtype = source_array.dtype
+    itemsize = dtype.itemsize
+
+    if target_chunks is None:
+        # this is just a pass-through copy
+        target_chunks = source_chunks
+
+    if isinstance(target_chunks, dict):
+        array_dims = _get_dims_from_zarr_array(source_array)
+        try:
+            target_chunks = _shape_dict_to_tuple(array_dims, target_chunks)
+        except KeyError:
+            raise KeyError(
+                "You must explicitly specify each dimension size in target_chunks. "
+                f"Got array_dims {array_dims}, target_chunks {target_chunks}."
+            )
+
+    # TODO: rewrite to avoid the hard dependency on dask
+    max_mem = cubed.vendor.dask.utils.parse_bytes(max_mem)
+
+    # don't consolidate reads for Dask arrays
+    consolidate_reads = isinstance(source_array, zarr.core.Array)
+    read_chunks, int_chunks, write_chunks = rechunking_plan(
+        shape,
+        source_chunks,
+        target_chunks,
+        itemsize,
+        max_mem,
+        consolidate_reads=consolidate_reads,
+    )
+
+    # create target
+    shape = tuple(int(x) for x in shape)  # ensure python ints for serialization
+    target_chunks = tuple(int(x) for x in target_chunks)
+    int_chunks = tuple(int(x) for x in int_chunks)
+    write_chunks = tuple(int(x) for x in write_chunks)
+
+    target_array = _zarr_empty(
+        shape,
+        target_store_or_group,
+        target_chunks,
+        dtype,
+        name=name,
+        **(target_options or {}),
+    )
+    try:
+        target_array.attrs.update(source_array.attrs)
+    except AttributeError:
+        pass
+
+    if read_chunks == write_chunks:
+        int_array = None
+    else:
+        # do intermediate store
+        if temp_store_or_group is None:
+            raise ValueError(
+                "A temporary store location must be provided{}.".format(
+                    f" (array={name})" if name else ""
+                )
+            )
+        int_array = _zarr_empty(
+            shape,
+            temp_store_or_group,
+            int_chunks,
+            dtype,
+            name=name,
+            **(temp_options or {}),
+        )
+
+    read_proxy = ArrayProxy(source_array, read_chunks)
+    int_proxy = ArrayProxy(int_array, int_chunks)
+    write_proxy = ArrayProxy(target_array, write_chunks)
+    return CopySpec(read_proxy, int_proxy, write_proxy)
 
 
 def total_chunks(shape, chunks):
