0984c3853b3d6d40cbe415d7fd58cc92e5addbe5
Tom White <tom.e.white@gmail.com>
2023-06-15 15:21:17 +0100

    Publish packages to PyPI using GitHub Actions

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="cubed",
-        packages=["cubed"],
         package_dir={
             "cubed": "cubed"
         },  # avoids pip install problems if one creates a tmp directory alongside cubed directory
