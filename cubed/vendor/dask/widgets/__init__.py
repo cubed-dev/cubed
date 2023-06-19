7473c3b3514d42286d06edd900b06a9a9a018db9
Thomas Nicholas <thomas.nicholas@columbia.edu>
2023-06-19 18:55:17 -0400

    vendor all parts of dask needed for html array repr

---
+++ cubed/vendor/dask/widgets/__init__.py
@@ -0,0 +1,24 @@
+try:
+    from cubed.vendor.dask.widgets.widgets import (
+        FILTERS,
+        TEMPLATE_PATHS,
+        get_environment,
+        get_template,
+    )
+
+except ImportError as e:
+    msg = (
+        "Cubed diagnostics requirements are not installed.\n\n"
+        "Please either conda or pip install as follows:\n\n"
+        "  conda install cubed                     # either conda install\n"
+        '  python -m pip install "cubed[diagnostics]" --upgrade  # or python -m pip install'
+    )
+    exception = e  # Explicit reference for e as it will be lost outside the try block
+    FILTERS = {}
+    TEMPLATE_PATHS = []
+
+    def get_environment():
+        raise ImportError(msg) from exception
+
+    def get_template(name: str):
+        raise ImportError(msg) from exception
