609882e0bad4bda0bcaa88a315b84b47e3535619
Thomas Nicholas <thomas.nicholas@columbia.edu>
2023-06-21 01:06:13 -0400

    add comment referring to dask license

--- cubed/tests/test_html.py
+++ cubed/tests/test_html.py
@@ -3,6 +3,7 @@ import pytest
 from cubed.array_api import ones
 
 
+# This test is adapted from dask, see the license in cubed/vendor/dask/LICENSE.txt
 def test_repr_html():
     pytest.importorskip("jinja2")
     assert ones([])._repr_html_()
