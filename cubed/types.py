849dc6182cd2e3b45facd745d0d74f8e41f00e88
Tom White <tom.e.white@gmail.com>
2023-06-26 15:12:07 +0100

    Add top-level types

---
+++ cubed/types.py
@@ -0,0 +1,21 @@
+from typing import Any, Dict, Literal, Tuple, Union
+
+T_Shape = Tuple[int, ...]
+
+T_DType = Any  # TODO: improve this
+
+# Regular chunks are where all chunks must be the same size, whereas
+# rectangular chunks may be different sizes, and contain the size of
+# each chunk along each axis.
+# Zarr currently supports only regular chunks, although rectangular chunks
+# are proposed in https://zarr.dev/zeps/draft/ZEP0003.html.
+# Dask supports rectangular chunks.
+# The general T_Chunks type is for specifying chunks, it is generally
+# converted to a T_RectangularChunks or T_RegularChunks internally.
+T_RegularChunks = Tuple[int, ...]
+T_RectangularChunks = Tuple[Tuple[int, ...], ...]
+T_Chunks = Union[
+    int, T_RegularChunks, T_RectangularChunks, Dict[Any, Any], Literal["auto"]
+]
+
+T_Store = str  # TODO: expand this
