d1191f124077b1d3b3ec6a37495295e4ed63353c
Alex Merose <al@merose.com>
2023-11-21 16:34:43 +1300

    Implementation of `std` and `var`.
    Fixes #29. Here, I use existing cubed operations to implement `var` and `std`. Please let me know if I should reimplement the primitives as pure reductions.

--- cubed/array_api/statistical_functions.py
+++ cubed/array_api/statistical_functions.py
@@ -16,6 +16,7 @@ from cubed.array_api.dtypes import (
     uint64,
 )
 from cubed.core import reduction
+from cubed.array_api.elementwise_functions import subtract, square, sqrt
 
 
 def max(x, /, *, axis=None, keepdims=False):
@@ -152,3 +153,12 @@ def sum(x, /, *, axis=None, dtype=None, keepdims=False):
         keepdims=keepdims,
         extra_func_kwargs=extra_func_kwargs,
     )
+
+
+def var(x, /, *, axis=None, keepdims=False):
+    mu = mean(x, axis=axis, keepdims=True)
+    return mean(square(subtract(x, mu)), axis=axis, keepdims=keepdims)
+
+
+def std(x, /, *, axis=None, keepdims=False):
+    return sqrt(var(x, axis=axis, keepdims=keepdims))
