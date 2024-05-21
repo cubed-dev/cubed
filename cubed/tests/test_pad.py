4cf93c9fd136bd245b4c24c79c785a440a091e8c
Tom White <tom.e.white@gmail.com>
2024-05-21 16:39:40 +0100

    Implement NumPy's `__array_function__` protocol for array methods that are not in the Array API Standard
    * nanmean, nansum, pad

--- cubed/tests/test_pad.py
+++ cubed/tests/test_pad.py
@@ -11,11 +11,15 @@ def spec(tmp_path):
     return cubed.Spec(tmp_path, allowed_mem=100000)
 
 
-def test_pad(spec):
+@pytest.mark.parametrize("namespace", [cubed, np])
+def test_pad(spec, namespace):
     an = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
     a = xp.asarray([[1, 2, 3], [4, 5, 6], [7, 8, 9]], chunks=(2, 2), spec=spec)
-    b = cubed.pad(a, ((1, 0), (0, 0)), mode="symmetric")
+    # check that we can dispatch via the numpy namespace (via __array_function__)
+    # since pad is not yet a part of the Array API Standard
+    b = namespace.pad(a, ((1, 0), (0, 0)), mode="symmetric")
+    assert isinstance(b, cubed.Array)
     assert b.chunks == ((2, 2), (2, 1))
 
     assert_array_equal(b.compute(), np.pad(an, ((1, 0), (0, 0)), mode="symmetric"))
