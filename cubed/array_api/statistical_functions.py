b8ce6ec2334c8fea4b9563ee2429b52f4eb0a7e1
Alex Merose <al@merose.com>
2024-08-06 20:31:38 +0100

    Inlined extracted function.
    Inlined in order to not violate API boundaries. Trying to put this in a good place ends up leading to a circular import issue.

--- cubed/array_api/statistical_functions.py
+++ cubed/array_api/statistical_functions.py
@@ -5,9 +5,18 @@ from cubed.array_api.dtypes import (
     _numeric_dtypes,
     _real_floating_dtypes,
     _real_numeric_dtypes,
+    _signed_integer_dtypes,
+    _unsigned_integer_dtypes,
+    complex64,
+    complex128,
+    float32,
+    float64,
+    int32,
+    uint32,
+    int64,
+    uint64,
 )
-from cubed.backend_array_api import namespace as nxp
-from cubed.array_api.utility_functions import operator_default_dtype
+from cubed.backend_array_api import namespace as nxp, PRECISION
 from cubed.core import reduction
 
 
@@ -122,7 +131,16 @@ def prod(
     if x.dtype not in _numeric_dtypes and x.dtype not in _boolean_dtypes:
         raise TypeError("Only numeric or boolean dtypes are allowed in prod")
     if dtype is None:
-        dtype = operator_default_dtype(x)
+        if x.dtype in _signed_integer_dtypes:
+            dtype = int64 if PRECISION == 64 else int32
+        elif x.dtype in _unsigned_integer_dtypes:
+            dtype = uint64 if PRECISION == 64 else uint32
+        elif x.dtype == float32 and PRECISION == 64:
+            dtype = float64
+        elif x.dtype == complex64 and PRECISION == 64:
+            dtype = complex128
+        else:
+            dtype = x.dtype
     extra_func_kwargs = dict(dtype=dtype)
     return reduction(
         x,
@@ -143,7 +161,16 @@ def sum(
     if x.dtype not in _numeric_dtypes and x.dtype not in _boolean_dtypes:
         raise TypeError("Only numeric or boolean dtypes are allowed in sum")
     if dtype is None:
-        dtype = operator_default_dtype(x)
+        if x.dtype in _signed_integer_dtypes:
+            dtype = int64 if PRECISION == 64 else int32
+        elif x.dtype in _unsigned_integer_dtypes:
+            dtype = uint64 if PRECISION == 64 else uint32
+        elif x.dtype == float32 and PRECISION == 64:
+            dtype = float64
+        elif x.dtype == complex64 and PRECISION == 64:
+            dtype = complex128
+        else:
+            dtype = x.dtype
     extra_func_kwargs = dict(dtype=dtype)
     return reduction(
         x,
