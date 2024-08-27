ff605e30886f112638af4555d88f45ed314654c5
Tom White <tom.e.white@gmail.com>
2024-08-27 15:48:43 +0100

    Memray integration (#558)
    * Use Memray to examine tasks running on `lithops` or `processes` executors
    
    * Add Memray documentation
    
    * Fix mypy
    
    * Update docs/user-guide/diagnostics.md
    
    Co-authored-by: Tom Nicholas <tom@cworthy.org>
    
    * Make memray check more defensive
    
    ---------
    
    Co-authored-by: Tom Nicholas <tom@cworthy.org>

--- cubed/runtime/utils.py
+++ cubed/runtime/utils.py
@@ -1,9 +1,16 @@
 import time
+from contextlib import nullcontext
 from functools import partial
 from itertools import islice
+from pathlib import Path
 
 from cubed.runtime.types import OperationStartEvent, TaskEndEvent
 from cubed.utils import peak_measured_mem
+
+try:
+    import memray
+except ImportError:
+    memray = None
 
 sym_counter = 0
 
@@ -37,6 +44,32 @@ def execution_stats(func):
     """Decorator to measure timing information and peak memory usage of a function call."""
 
     return partial(execute_with_stats, func)
+
+
+def execute_with_memray(function, input, **kwargs):
+    # only run memray if installed, and only for first input (for operations that run on block locations)
+    if (
+        memray is not None
+        and "compute_id" in kwargs
+        and isinstance(input, list)
+        and all(isinstance(i, int) for i in input)
+        and sum(input) == 0
+    ):
+        compute_id = kwargs["compute_id"]
+        name = kwargs["name"]
+        memray_dir = Path(f"history/{compute_id}/memray")
+        memray_dir.mkdir(parents=True, exist_ok=True)
+        cm = memray.Tracker(memray_dir / f"{name}.bin")
+    else:
+        cm = nullcontext()
+    with cm:
+        result = result = function(input, **kwargs)
+        return result
+
+
+def profile_memray(func):
+    """Decorator to profile a function call with memray."""
+    return partial(execute_with_memray, func)
 
 
 def handle_operation_start_callbacks(callbacks, name):
