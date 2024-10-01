03f3e0ec384cb3631fa3e545705c25589234a0d5
Tom White <tom.e.white@gmail.com>
2024-10-01 09:52:16 +0100

    Remove deprecated code (#588)

--- cubed/extensions/rich.py
+++
@@ -1,8 +0,0 @@
-import warnings
-
-warnings.warn(
-    "`cubed.extensions.rich` is deprecated, please use `cubed.diagnostics.rich` instead",
-    DeprecationWarning,
-)
-
-from cubed.diagnostics.rich import RichProgressBar  # noqa: F401
