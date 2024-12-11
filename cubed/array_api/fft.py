37fee5b68921014481bd4c5b4e19d485fda23bba
Tom White <tom.e.white@gmail.com>
2024-12-11 13:55:41 +0000

    ifft

--- cubed/array_api/fft.py
+++ cubed/array_api/fft.py
@@ -3,9 +3,17 @@ from cubed.core.ops import map_blocks
 
 
 def fft(x, /, *, n=None, axis=-1, norm="backward"):
+    return fft_1d(nxp.fft.fft, x, n=n, axis=axis, norm=norm)
+
+
+def ifft(x, /, *, n=None, axis=-1, norm="backward"):
+    return fft_1d(nxp.fft.ifft, x, n=n, axis=axis, norm=norm)
+
+
+def fft_1d(fft_func, x, /, *, n=None, axis=-1, norm="backward"):
     if x.numblocks[axis] > 1:
         raise ValueError(
-            "FFT is only supported along axes with a single chunk. "
+            "FFT can only be applied along axes with a single chunk. "
             # TODO: give details about what was tried and mention rechunking (see qr message)
         )
 
@@ -20,11 +28,12 @@ def fft(x, /, *, n=None, axis=-1, norm="backward"):
         x,
         dtype=nxp.complex128,
         chunks=chunks,
+        fft_func=fft_func,
         n=n,
         axis=axis,
         norm=norm,
     )
 
 
-def _fft(a, n=None, axis=None, norm=None):
-    return nxp.fft.fft(a, n=n, axis=axis, norm=norm)
+def _fft(a, fft_func=None, n=None, axis=None, norm=None):
+    return fft_func(a, n=n, axis=axis, norm=norm)
--- cubed/tests/test_fft.py
+++ cubed/tests/test_fft.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pytest
 from numpy.testing import assert_array_equal
 
@@ -7,18 +6,22 @@ import cubed.array_api as xp
 from cubed.backend_array_api import namespace as nxp
 
 
+@pytest.mark.parametrize("funcname", ["fft", "ifft"])
 @pytest.mark.parametrize("n", [None, 5, 13])
-def test_fft(n):
-    an = np.arange(100).reshape(10, 10)
-    bn = nxp.fft.fft(an, n=n)
+def test_fft(funcname, n):
+    nxp_fft = getattr(nxp.fft, funcname)
+    cb_fft = getattr(xp.fft, funcname)
+
+    an = nxp.arange(100).reshape(10, 10)
+    bn = nxp_fft(an, n=n)
     a = cubed.from_array(an, chunks=(1, 10))
-    b = xp.fft.fft(a, n=n)
+    b = cb_fft(a, n=n)
 
     assert_array_equal(b.compute(), bn)
 
 
 def test_fft_chunked_axis_fails():
-    an = np.arange(100).reshape(10, 10)
+    an = nxp.arange(100).reshape(10, 10)
     a = cubed.from_array(an, chunks=(1, 10))
 
     with pytest.raises(ValueError):
