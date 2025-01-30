e755f78f39378d1957d7265994b68da7de64374e
Tom White <tom.e.white@gmail.com>
2025-01-30 15:37:54 +0000

    Globus Compute executor

--- cubed/tests/utils.py
+++ cubed/tests/utils.py
@@ -1,3 +1,4 @@
+import os
 import platform
 from typing import Iterable
 
@@ -41,6 +42,13 @@ except ImportError:
 try:
     ALL_EXECUTORS.append(create_executor("dask"))
     MAIN_EXECUTORS.append(create_executor("dask"))
+except ImportError:
+    pass
+
+try:
+    executor_options = dict(endpoint_id=os.getenv("ENDPOINT_ID", None))
+    ALL_EXECUTORS.append(create_executor("globus-compute", executor_options))
+    MAIN_EXECUTORS.append(create_executor("globus-compute", executor_options))
 except ImportError:
     pass
 
