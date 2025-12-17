30d09fd1525a79cef58ae4a0bd16ef12931b12a3
Tom White <tom.e.white@gmail.com>
2025-12-17 15:00:29 +0000

    Simple implementation of update operations append and set
    Implement uneven append
    
    Rename set_ to set_scalar

---
+++ cubed/array/update.py
@@ -0,0 +1,99 @@
+import ndindex
+
+from cubed.array_api.array_object import Array
+from cubed.array_api.manipulation_functions import concat
+from cubed.core.ops import _create_zarr_indexer, _store_array, map_blocks
+from cubed.storage.store import is_storage_array
+
+
+def append(a, array, /, *, axis=0):
+    """Append array to Zarr array a along axis"""
+
+    z = a._zarray
+
+    if not is_storage_array(z):
+        raise ValueError("Array must be a Zarr array to perform in-place set operation")
+
+    # these are checks from concat
+    arrays = [z, array]
+
+    if len({a.dtype for a in arrays}) > 1:
+        raise ValueError("append inputs must all have the same dtype")
+
+    ndim = z.ndim
+    if not all(
+        i == axis or all(x.shape[i] == arrays[0].shape[i] for x in arrays)
+        for i in range(ndim)
+    ):
+        raise ValueError(
+            f"all the input array dimensions except for the append axis must match exactly: {[x.shape for x in arrays]}"
+        )
+
+    shape_axis = z.shape[axis]
+    offset = shape_axis % z.chunks[axis]
+
+    # find region to write update array to
+    region = tuple(
+        slice(z.shape[i] - offset, z.shape[i] + s) if i == axis else slice(0, s)
+        for i, s in enumerate(array.shape)
+    )
+
+    # find new shape of zarr array (z)
+    new_shape = tuple(
+        s + array.shape[axis] if i == axis else s for i, s in enumerate(z.shape)
+    )
+    z.resize(new_shape)
+
+    if offset == 0:
+        # note the returned array should only really have compute called on it
+        # TODO: change to_zarr to have a compute arg, which if False returns the array
+        return _store_array(array, z, region=region)
+    else:
+        # TODO: fix https://github.com/cubed-dev/cubed/issues/414 to avoid a write to disk
+        idx = (slice(None),) * axis + (
+            slice(shape_axis - offset, shape_axis),
+            Ellipsis,
+        )
+        end_part = a[idx]
+        array_with_end_part = concat([end_part, array], axis=axis)
+
+        return _store_array(array_with_end_part, z, region=region)
+
+
+def set_scalar(source: "Array", key, value):
+    """Set scalar value on Zarr array indexing by key."""
+
+    # check that value is a scalar, so we don't have to worry about chunk selection, broadcasting, etc
+    if isinstance(value, Array):
+        raise NotImplementedError("Only scalar values are supported for set")
+
+    if not is_storage_array(source._zarray):
+        raise ValueError("Array must be a Zarr array to perform in-place set operation")
+
+    target = source._zarray  # Note: in place!
+    chunks = target.chunks
+    idx = ndindex.ndindex(key)
+    idx = idx.expand(source.shape)
+    selection = idx.raw
+    indexer = _create_zarr_indexer(selection, source.shape, source.chunksize)
+    output_blocks = map(
+        lambda chunk_projection: list(chunk_projection[0]), list(indexer)
+    )
+    chunk_selections = {cp.chunk_coords: cp.chunk_selection for cp in indexer}
+
+    # note the returned array should only really have compute called on it
+    return map_blocks(
+        _set,
+        source,
+        dtype=source.dtype,
+        chunks=chunks,
+        target_store=target,
+        output_blocks=output_blocks,
+        value=value,
+        chunk_selections=chunk_selections,
+    )
+
+
+def _set(a, value=None, chunk_selections=None, block_id=None):
+    a[chunk_selections[block_id]] = value
+    return a
---
+++ cubed/tests/test_update.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pytest
+
+import cubed
+import cubed.array_api as xp
+from cubed._testing import assert_array_equal
+from cubed.array.update import append, set_scalar
+from cubed.storage.store import open_storage_array
+from cubed.tests.utils import create_zarr
+
+
+@pytest.mark.parametrize("axis", [0, 1])
+def test_append(tmp_path, axis):
+    store = tmp_path / "a.zarr"
+    create_zarr(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        chunks=(3, 3),
+        store=store,
+    )
+
+    a = cubed.from_zarr(store, mode="r+")
+    b = xp.full((3, 3), 2, chunks=(3, 3))
+    c = append(a, b, axis=axis)
+
+    c.compute(_return_in_memory_array=False)  # don't load into memory
+
+    res = open_storage_array(store, mode="r")
+
+    assert_array_equal(
+        res[:],
+        np.concatenate(
+            [np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), np.full((3, 3), 2)], axis=axis
+        ),
+    )
+
+
+@pytest.mark.parametrize("axis", [0, 1])
+def test_append_uneven(tmp_path, axis):
+    store = tmp_path / "a.zarr"
+    create_zarr(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+        chunks=(2, 2),
+        store=store,
+    )
+
+    a = cubed.from_zarr(store, mode="r+")
+    b = xp.full((3, 3), 2, chunks=(2, 2))
+    c = append(a, b, axis=axis)
+
+    c.visualize(optimize_graph=True)
+    c.compute(_return_in_memory_array=False)  # don't load into memory
+
+    res = open_storage_array(store, mode="r")
+
+    assert_array_equal(
+        res[:],
+        np.concatenate(
+            [np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), np.full((3, 3), 2)], axis=axis
+        ),
+    )
+
+
+def test_set_scalar(tmp_path):
+    store = tmp_path / "a.zarr"
+    create_zarr(
+        [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]],
+        chunks=(2, 2),
+        store=store,
+    )
+    a = cubed.from_zarr(store, mode="r+")
+    c = set_scalar(a, (slice(None), 2), -1)
+    c.compute(_return_in_memory_array=False)  # don't load into memory
+    # za[(slice(None), 2)] = -1  # direct Zarr way (not distributed)
+
+    res = open_storage_array(store, mode="r")
+
+    assert_array_equal(
+        res[:],
+        np.array([[1, 2, -1, 4], [5, 6, -1, 8], [9, 10, -1, 12], [13, 14, -1, 16]]),
+    )
