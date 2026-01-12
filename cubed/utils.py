88e81a1f381dc56f5824796add69d1428bdd173b
TomNicholas <tom@earthmover.io>
2026-01-12 15:27:13 -0500

    forgot DataArrays don't have a ._variable attribute

--- cubed/utils.py
+++ cubed/utils.py
@@ -239,7 +239,7 @@ def extract_array_names(frame: FrameType) -> dict[str, str]:
             and obj.__class__.__name__ == "DataArray"
         ):
             # Note: We use ._data here instead of .data to avoid eager loading inside xarray.open_dataset internals - see https://github.com/cubed-dev/cubed/issues/855
-            if isinstance(obj._data, Array):
+            if isinstance(obj.variable._data, Array):
                 array_names_to_variable_names[obj._data.name] = name
         elif (
             type(obj).__module__.split(".")[0] == "xarray"
